52d1baf251344a2394e1d8e08b089fc80848a95b
juliabenedicto <74271084+juliabenedicto@users.noreply.github.com>
2021-05-24 20:23:16 +0200

    Colors Theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-    purge: ["./src/**/*.html"],
+    purge: ['./src/**/*.html',
+        './src/**/*.js',
+    ],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px',
+        },
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: colors.black,
+            white: colors.white,
+            gray: colors.trueGray,
+            primary: colors.pink,
+            secondary: colors.purple,
+        },
+        fontFamily: {
+            sans: ['Graphik', 'sans-serif'],
+            serif: ['Merriweather', 'serif'],
+        },
+        extend: {
+
+        }
     },
     variants: {
         extend: {},
